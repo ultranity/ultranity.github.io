1df4b179129661496639e5948fc21d73b221b928
ultranity <zjy1095429904@sina.com>
2018-01-03 15:22:26 +0800

    Site updated: 2018-01-03 15:22:23

--- js/src/title.js
+++ js/src/title.js
@@ -1,3 +1,16 @@
-// build time:Wed Jan 03 2018 14:41:04 GMT+0800 (中国标准时间)
-var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/favicon.ico");document.title="页面崩溃啦 ~ | 你快回来！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title=" 噫又好了~"+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
-//rebuild by neat 
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '页面崩溃啦 ~ | 你快回来！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = ' 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
